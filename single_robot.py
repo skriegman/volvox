10b9a14a7cccd90ee32a09ff6807c8f901dbc989
Sam <skriegman@gmail.com>
2022-08-09 15:17:02 -0400

    randomize light

--- single_robot.py
+++ single_robot.py
@@ -14,10 +14,11 @@ DRAW_LIGHT_SOURCE = True
 
 LIGHT_SIZE = 3
 
-INCLUDE_WALLS = True
+USE_TWO_LIGHTS = True
+
 WORLD_WIDTH = 21
 
-DRAW_WALLS = False  # make sure to turn on collisions in base.vxa:line 64 <EnableCollision>1</EnableCollision>
+USE_WALLS = False  # make sure to turn on collisions in base.vxa:line 64 <EnableCollision>1</EnableCollision>
 
 BODY_DIAMETER = 5  # in voxels
 
@@ -60,21 +61,29 @@ sphere[dist2 <= radius**2] = 1
 for layer in range(bz):
     body[:, :, layer] *= sphere[1:bx+1, 1:by+1, layer+1]
 
-# get random cilia field
-# body_cilia = 2*np.random.rand(bx,by,bz,3)-1
+# add perpendicular cilia field
+body_cilia = restricted_cilia(body, DEBUG=True)  # DEBUG means all cilia apply perpendicular force
+# debug cilia field:
+# body_cilia = 2*np.random.rand(bx,by,bz,3)-1  # random cilia
 # body_cilia[:,:,:,2] = 0
-body_cilia = restricted_cilia(body, DEBUG=True)  # different cilia
+# body_cilia = np.ones((bx,by,bz,3))
+# body_cilia[:,2:,2:,:] = 0
+# body_cilia[:2,:2,:1,:1] = -1
 
 # how sensitive should each voxel be to light? Note: only the surface voxels matter.
-body_photosensitivity = np.ones((bx,by,bz))
-# body_photosensitivity = np.random.rand(bx,by,bz)
+# body_activation_time_constant = np.ones((bx,by,bz)) * 6
+# body_recovery_time_constant = np.ones((bx,by,bz)) * 0.2
+# body_downregulation_constant = np.ones((bx,by,bz)) * 0.4
+body_activation_time_constant = 10 * np.random.rand(bx,by,bz)
+body_recovery_time_constant = 0.01 * np.random.rand(bx,by,bz)
+body_downregulation_constant = 0.1 * np.random.rand(bx,by,bz)
 
 # world
 world = body
 l_size = LIGHT_SIZE
 wx,wy,wz = LIGHT_X+bx, LIGHT_Y+by, max(LIGHT_Z+l_size//2+1, bz)
 
-if INCLUDE_WALLS:
+if USE_TWO_LIGHTS:
     wy = WORLD_WIDTH
     LIGHT_X += bx-2  # light embedded in the wall
     LIGHT_Y = WORLD_WIDTH//2
@@ -93,7 +102,7 @@ if DRAW_LIGHT_SOURCE > 0:
     print("lightB pos: " + str(lx+l_size/2-0.5) + ", " + str(ly+l_size/2-0.5) + ", " + str(lz+l_size/2-0.5) )
     world[lx:lx+l_size, ly:ly+l_size, lz:lz+l_size] = np.ones((l_size,)*3, dtype=np.int8) * 3  # light bulb B is mat 3
 
-if DRAW_WALLS:
+if USE_WALLS:
     # add walls to world
     world[:2, :, :bz+2] = 4  # wall
     world[:, :2, :bz+2] = 4
@@ -101,18 +110,27 @@ if DRAW_WALLS:
     world[:, -2:, :bz+2] = 4    
     # world[3*wx//4:3*wx//4+2, :, :bz+2] = 4  ## debug collisions
 
-
-# reshape cilia field for world
+# reshape body data for world
 world_cilia = np.zeros((wx,wy,wz, 3))
 world_cilia[wx//2-bx//2:wx//2+bx//2+1, wy//2-by//2:wy//2+by//2+1, :bz, :] = body_cilia
 world_cilia = np.swapaxes(world_cilia, 0,2)  # looks good; todo: test
 world_cilia = world_cilia.reshape(wz, 3*wx*wy)
 
-# reformat photosensitivity
-world_photosensitivity = np.zeros((wx,wy,wz))
-world_photosensitivity[wx//2-bx//2:wx//2+bx//2+1, wy//2-by//2:wy//2+by//2+1, :bz] = body_photosensitivity
-world_photosensitivity = np.swapaxes(world_photosensitivity, 0,2)  # looks good; todo: test
-world_photosensitivity = world_photosensitivity.reshape(wz, wx*wy)
+world_activation_time_constant = np.zeros((wx,wy,wz))
+world_activation_time_constant[wx//2-bx//2:wx//2+bx//2+1, wy//2-by//2:wy//2+by//2+1, :bz] = body_activation_time_constant
+world_activation_time_constant = np.swapaxes(world_activation_time_constant, 0,2) 
+world_activation_time_constant = world_activation_time_constant.reshape(wz, wx*wy)
+
+world_recovery_time_constant = np.zeros((wx,wy,wz))
+world_recovery_time_constant[wx//2-bx//2:wx//2+bx//2+1, wy//2-by//2:wy//2+by//2+1, :bz] = body_recovery_time_constant
+world_recovery_time_constant = np.swapaxes(world_recovery_time_constant, 0,2)  
+world_recovery_time_constant = world_recovery_time_constant.reshape(wz, wx*wy)
+
+world_downregulation_constant = np.zeros((wx,wy,wz))
+world_downregulation_constant[wx//2-bx//2:wx//2+bx//2+1, wy//2-by//2:wy//2+by//2+1, :bz] = body_downregulation_constant
+world_downregulation_constant = np.swapaxes(world_downregulation_constant, 0,2) 
+world_downregulation_constant = world_downregulation_constant.reshape(wz, wx*wy)
+
 
 # reformat data for voxcraft
 world = np.swapaxes(world, 0,2)
@@ -194,10 +212,22 @@ for i in range(world_cilia.shape[0]):
     str_layer = "".join([str(c) + ", " for c in world_cilia[i]]) # other variables can be floats so they need commas
     layer.text = etree.CDATA(str_layer)
 
-data = etree.SubElement(structure, "Photosensitivity")
-for i in range(world_photosensitivity.shape[0]):
+data = etree.SubElement(structure, "ActivationTimeConstant")
+for i in range(world_activation_time_constant.shape[0]):
     layer = etree.SubElement(data, "Layer")
-    str_layer = "".join([str(c) + ", " for c in world_photosensitivity[i]]) # other variables can be floats so they need commas
+    str_layer = "".join([str(c) + ", " for c in world_activation_time_constant[i]])
+    layer.text = etree.CDATA(str_layer)
+
+data = etree.SubElement(structure, "RecoveryTimeConstant")
+for i in range(world_recovery_time_constant.shape[0]):
+    layer = etree.SubElement(data, "Layer")
+    str_layer = "".join([str(c) + ", " for c in world_recovery_time_constant[i]])
+    layer.text = etree.CDATA(str_layer)
+
+data = etree.SubElement(structure, "DownregulationConstant")
+for i in range(world_downregulation_constant.shape[0]):
+    layer = etree.SubElement(data, "Layer")
+    str_layer = "".join([str(c) + ", " for c in world_downregulation_constant[i]])
     layer.text = etree.CDATA(str_layer)
 
 # save the vxd to data folder
